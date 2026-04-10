0ee15d803a17a616336621002305b2f2071378c0
Sauvik-14 <zenx.official5914@gmail.com>
2026-04-10 03:23:04 +0000

    UC6: Bogie → Capacity (HashMap)

---
+++ TrainAppUC6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TrainAppUC6 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("Sleeper", 72);
+        map.put("AC Chair", 56);
+        map.put("First Class", 24);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
